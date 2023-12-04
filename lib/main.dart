48cca4c1164919e3ea761853bcef912f7db84072
anibrahim <anibrahim@dev.thqah.sa>
2023-12-04 17:43:53 +0300

    fix navigation rail

--- lib/main.dart
+++ lib/main.dart
@@ -29,60 +29,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
-
-    var _selectedIndex = 0;
+    int _selectedIndex = 0;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      bottomNavigationBar: NavigationRailTheme(
-        data: NavigationRailThemeData(
-          backgroundColor: Colors.amber.shade50,
-          selectedIconTheme: IconThemeData(color: Colors.deepPurple.shade700),
-          selectedLabelTextStyle: TextStyle(color: Colors.deepPurple.shade700),
-          unselectedIconTheme: IconThemeData(color: Colors.blueGrey.shade600),
-          unselectedLabelTextStyle: TextStyle(color: Colors.blueGrey.shade600),
-        ),
-        child: NavigationRail(
-          destinations: const [
-            NavigationRailDestination(
-              icon: Icon(Icons.favorite_border),
-              selectedIcon: Icon(Icons.favorite),
-              label: Text('First'),
+      body: Row(
+        children: [
+          NavigationRailTheme(
+            data: NavigationRailThemeData(
+              backgroundColor: Colors.blueGrey.shade50,
+              selectedIconTheme:
+                  IconThemeData(color: Colors.deepPurple.shade700),
+              selectedLabelTextStyle:
+                  TextStyle(color: Colors.deepPurple.shade700),
+              unselectedIconTheme:
+                  IconThemeData(color: Colors.blueGrey.shade600),
+              unselectedLabelTextStyle:
+                  TextStyle(color: Colors.blueGrey.shade600),
             ),
-            NavigationRailDestination(
-              icon: Icon(Icons.bookmark_border),
-              selectedIcon: Icon(Icons.book),
-              label: Text('Second'),
+            child: NavigationRail(
+              destinations: const [
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite_border),
+                  selectedIcon: Icon(Icons.favorite),
+                  label: Text('First'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.bookmark_border),
+                  selectedIcon: Icon(Icons.book),
+                  label: Text('Second'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.star_border),
+                  selectedIcon: Icon(Icons.star),
+                  label: Text('Third'),
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              labelType: NavigationRailLabelType.all,
             ),
-            NavigationRailDestination(
-              icon: Icon(Icons.star_border),
-              selectedIcon: Icon(Icons.star),
-              label: Text('Third'),
+          ),
+          const VerticalDivider(thickness: 1, width: 1),
+          // This is the main content.
+          Expanded(
+            child: Center(
+              child: Text('selectedIndex: $_selectedIndex'),
             ),
-          ],
-          selectedIndex: _selectedIndex,
-          onDestinationSelected: (int index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-          labelType: NavigationRailLabelType.all,
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Navigation Rail Demo',
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
